7acefc2309497cd35b9fb8a5e185454553413a8c
zhouMac <LazyZhou1997@gmail.com>
2018-03-13 23:29:08 +0800

    初始化项目

---
+++ src/main/java/edu/scu/my_shop/MyShopApplication.java
@@ -0,0 +1,12 @@
+package edu.scu.my_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyShopApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/scu/my_shop/MyShopApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.scu.my_shop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
