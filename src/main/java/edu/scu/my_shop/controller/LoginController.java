5bba28e86459a570cd527e47920aaa44163e445e
zhouMac <LazyZhou1997@gmail.com>
2018-03-15 21:06:24 +0800

    初始化

---
+++ src/main/java/edu/scu/my_shop/controller/LoginController.java
@@ -0,0 +1,15 @@
+package edu.scu.my_shop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+
+
+    public void login(){
+
+    }
+}
