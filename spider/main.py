43e74ef54c0ff8af579cd09958c41097808bd834
Hzzone <zhizhonghwang@gmail.com>
2018-03-19 15:50:47 +0800

    remember password

--- spider/main.py
+++ spider/main.py
@@ -2,6 +2,8 @@ import requests
 import re
 import pymysql
 from bs4 import BeautifulSoup
+import json
+import time
 
 #创建连接
 conn = pymysql.connect(
@@ -23,20 +25,6 @@ product_dict = {
     "食品酒类": ["水果", "肉类", "饮料", "名酒"],
 }
 
-def insertIntoDatabase():
-    # 创建游标
-    cursor = conn.cursor()
-    for key in product_dict:
-        first_categort_id = genRandomString()
-        cursor.execute("INSERT INTO first_category VALUE (%s, %s);", (key, first_categort_id, ))
-        for second_category_name in product_dict[key]:
-            second_category_id = genRandomString()
-            cursor.execute("INSERT INTO second_category VALUE (%s, %s, %s);", (second_category_id, first_categort_id, second_category_name, ))
-
-    conn.commit()
-    cursor.close()
-
-
 class Product:
     def __init__(self, product_id):
         self.product_id = product_id
@@ -47,6 +35,9 @@ class Product:
         self.second_category_name = ''
         self.product_price = 100.0
 
+    def get(self):
+        return (self.product_id, self.product_name, self.second_category_name, self.product_price, self.product_description, self.left_totals, )
+
 
 def genRandomString(length=50):
     import random
@@ -56,37 +47,100 @@ def genRandomString(length=50):
 def searchByKeyword(keyword, total_page=2):
     url_list = []
     for page_number in range(1, total_page+1):
-        url = "https://www.amazon.cn/s/ref=sr_pg_2?rh=i:aps,k:手机&page=%s&keywords=%s&ie=UTF8" % (page_number, keyword)
-        response = requests.get(url)
-        pattern = re.compile('<a class="a-link-normal a-text-normal" target="_blank" href="(.*)"><img alt=')
-        result = re.findall(pattern, response.content.decode())
-        [url_list.append(x) for x in result]
+        url = "https://www.amazon.cn/s/ref=sr_pg_2?rh=i:aps,k:%s&page=%s&keywords=%s&ie=UTF8" % (keyword, page_number, keyword)
+        content = requests.get(url).content.decode()
+        soup = BeautifulSoup(content)
+        result = soup.find_all('a', class_=['a-link-normal', 'a-text-normal'])
+        # pattern = re.compile('<a class="a-link-normal a-text-normal" target="_blank" href="(.*)"><img alt=')
+        # result = re.findall(pattern, content)
+        [url_list.append(x['href']) for x in result]
+        # [url_list.append(x['href']) for x in result]
+        time.sleep(1)
+
+    print(keyword)
 
     return set(url_list)
 
 
 def getProductInfo(product_url, second_category_name):
-    content = requests.get(product_url).content.decode()
-    product = Product(genRandomString())
-    soup = BeautifulSoup(content)
-    product.product_name = soup.find_all(id="productTitle")[0].text.strip()
-    product.second_category_name = second_category_name
-    print(soup.find_all("spam", id="priceblock_ourprice"))
-    product.product_price = float(re.sub("\D", "", soup.find(id="priceblock_ourprice")[0].text.strip()))
+    try:
+        content = requests.get(product_url).content.decode()
+        product = Product(genRandomString())
+        soup = BeautifulSoup(content)
+        product.product_name = soup.find_all(id="productTitle")[0].text.strip()
+        # print(product.product_name)
+        product.second_category_name = second_category_name
+        price_str = re.sub(",", "", soup.find_all("span", class_=['a-size-medium', 'a-color-price'])[0].text.strip()[1:])
+        product.product_price = float(price_str)
+        # img_url = re.findall('<img alt="" src="(.*)._SX38_SY50_CR,0,0,38,50_.jpg"/>]', content)
+        # rrr= soup.find_all(id='a-autoid-8-announce')
+        for span in soup.find_all('span', class_=['a-button-text']):
+            img = span.find_all('img')
+            if len(img):
+                product.image_list.append(img[0]['src'].strip('._SX38_SY50_CR,0,0,38,50_.jpg')+'._SL800_.jpg')
 
-    return product
-    # 522_image = "https://images-cn.ssl-images-amazon.com/images/I/51TEg+W1cDL._SX522_.jpg"
+        des = {}
+        table = soup.find_all('div', class_=['pdTab', ])[0]
+        labels = table.find_all('td', class_=['label', ])
+        values = table.find_all('td', class_=['value', ])
+        for x, y in zip(labels, values):
+            des[x.text] = y.text
+        product.product_description = json.dumps(des, ensure_ascii=False)
+        time.sleep(1)
+        print(product.product_name)
+        return product
+    except:
+        return None
+
+
+
+
+
+def insertIntoDatabase():
+    # 创建游标
+    cursor = conn.cursor()
+    i = 1
+    for key in product_dict:
+        # first_categort_id = genRandomString()
+        # cursor.execute("INSERT INTO first_category VALUE (%s, %s);", (key, first_categort_id, ))
+        for second_category_name in product_dict[key]:
+            # second_category_id = genRandomString()
+            rows = cursor.execute("SELECT second_category_id FROM second_category WHERE second_category.second_category_name=%s;", (second_category_name))
+            second_category_id = cursor.fetchone()[0]
+            # cursor.execute("INSERT INTO second_category VALUE (%s, %s, %s);", (second_category_id, first_categort_id, second_category_name, ))
+            for url in searchByKeyword(second_category_name, total_page=3):
+                p = getProductInfo(url, second_category_name)
+                if p:
+                    cursor.execute("INSERT INTO product VALUE (%s, %s, %s, %s, %s, %s);", (p.product_id,
+                                   p.product_name, second_category_id, p.product_price, p.product_description, p.left_totals))
+                    for img in p.image_list:
+                        try:
+                            cursor.execute("INSERT INTO image VALUE (%s, %s, %s);", (img, p.product_id, img))
+                        except:
+                            pass
+
+                    # print(i, p.get())
+                    print(i)
+
+                    conn.commit()
+                    i = i+1
+                else:
+                    continue
+    time.sleep(10)
+
+    cursor.close()
+
 
 if __name__ == "__main__":
     # result = searchByKeyword("手机")
     # print(result)
     # print(len(result))
 
-    product = getProductInfo("https://www.amazon.cn/dp/B07989DSVQ", "手机")
+    # product = getProductInfo("https://www.amazon.cn/dp/B07989DSVQ", "手机")
+    #
+    # print(product.product_description)
 
-    print(product)
-
-    # insertIntoDatabase()
+    insertIntoDatabase()
 
 
 
