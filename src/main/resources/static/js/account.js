275629f983c3e5ffb61a2cbcf9f7cfa2ad6ddbfe
Royisfan <171482382@qq.com>
2018-03-20 11:18:27 +0800

    显示用户个人信息

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,14 +1,15 @@
 $(function () {
     $.getJSON("http://localhost:8080//getUserInfo",function(data){
         var username = data.data.userName;
-        console.log(username);
         var userimg = data.data.headImg;
-        console.log(userimg);
         var useremail = data.data.userId;
-        console.log(useremail);
         var userdate = data.data.birthday;
-        console.log(userdate);
 
-
+        $('#user-info-name').attr('placeholder',username);
+        $('#user-info-email').attr('placeholder',useremail);
+        if(null != userdate ){
+            $('#user-info-date').attr('placeholder',userdate);
+        }
+        $('#user-info-image').attr('src',userimg);
     })
 });
