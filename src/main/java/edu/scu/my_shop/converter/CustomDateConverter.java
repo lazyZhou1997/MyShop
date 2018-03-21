380b8972d84efa8c6fd7a824096f560432f98833
zhouMac <LazyZhou1997@gmail.com>
2018-03-21 22:05:26 +0800

    完成修改信息功能

---
+++ src/main/java/edu/scu/my_shop/converter/CustomDateConverter.java
@@ -0,0 +1,47 @@
+package edu.scu.my_shop.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 字符串转日期的转换器
+ */
+@Component
+public class CustomDateConverter implements Converter<String, Date> {
+    private static final String dateFormat      = "yyyy-MM-dd HH:mm:ss";
+    private static final String shortDateFormat = "yyyy-MM-dd";
+
+    /**
+     * @param source
+     * @return
+     */
+    @Override
+    public Date convert(String source) {
+        if (StringUtils.isEmpty(source)) {
+            return null;
+        }
+        source = source.trim();
+        try {
+            if (source.contains("-")) {
+                SimpleDateFormat formatter;
+                if (source.contains(":")) {
+                    formatter = new SimpleDateFormat(dateFormat);
+                } else {
+                    formatter = new SimpleDateFormat(shortDateFormat);
+                }
+                Date dtDate = formatter.parse(source);
+                return dtDate;
+            } else if (source.matches("^\\d+$")) {
+                Long lDate = new Long(source);
+                return new Date(lDate);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("parser %s to Date fail", source));
+        }
+        throw new RuntimeException(String.format("parser %s to Date fail", source));
+    }
+}
