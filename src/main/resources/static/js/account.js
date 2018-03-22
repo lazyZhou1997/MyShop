362b28803390dd440815a0c049de01e88c749f84
Royisfan <171482382@qq.com>
2018-03-22 11:45:18 +0800

    search

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -1,6 +1,22 @@
 //用户信息
-$(function () {
-    $.getJSON("http://localhost:8080//getUserInfo",function(data){
+// $(function () {
+//     $.getJSON("http://localhost:8080//getUserInfo",function(data){
+//         var username = data.data.userName;
+//         var userimg = data.data.headImg;
+//         var useremail = data.data.userId;
+//         var userdate = data.data.birthday;
+//
+//         // 打印用户信息
+//         $('#user-info-name').attr('placeholder',username);
+//         $('#user-info-email').attr('placeholder',useremail);
+//         if(null != userdate ){
+//             $('#user-info-date').attr('placeholder',userdate);
+//         }
+//         $('#user-info-image').attr('src',userimg);
+//     })
+// });
+  $(function() {
+    $.getJSON("http://localhost:8080/getUserInfo",function(data){
         var username = data.data.userName;
         var userimg = data.data.headImg;
         var useremail = data.data.userId;
@@ -14,11 +30,11 @@ $(function () {
         }
         $('#user-info-image').attr('src',userimg);
     })
-});
+})
 
 function check() {
  var username = $('#user-info-name').val();
- // var userdate = $('#user-info-date').val();
+ var userdate = $('#user-info-date').val();
  var userpsw1 = $('#user-info-psw1').val();
  var userpsw2 = $('#user-info-psw2').val();
  var DATE_FORMAT = /^[0-9]{4}-[0-1]?[0-9]{1}-[0-3]?[0-9]{1}$/; //日期的正则表达式
@@ -34,10 +50,10 @@ function check() {
      return false;
  }
  //输入的格式不能错
- // else if(userdate && !DATE_FORMAT.test(userdate)){
- //     swal("","输入的日期格式有误，请按照xxxx-xx-xx的格式输入","error");
- //     return false;
- // }
+ else if(userdate && !DATE_FORMAT.test(userdate)){
+     swal("","输入的日期格式有误，请按照xxxx-xx-xx的格式输入","error");
+     return false;
+ }
 
 }
 
--- src/main/resources/static/js/search-result.js
+++ src/main/resources/static/js/search-result.js
@@ -14,25 +14,25 @@
 // </div>
 
 $(function () {
-    $.getJSON("url of search result",function (data) {
-        var product_image = data.image;
-        var product_description = data.description;
-        var product_price = data.price;
-
-        for(var i = 0; i<data.length;i++){
-            var div = "<div class=\"col-sm-2\">";
-            div += "<div class=\"product-image-wrapper\">";
-            div += " <div class=\"single-products\">";
-            div += "<div class=\"productinfo text-center\">";
-            div += "<a href=\"#\"><img alt = '' src='" + product_image + "'/></a>";
-            div += "<h2>" + product_price + "</h2>";
-            div += "<p>" + product_description + "</p>";
-            div += "<a href=\"#\" class=\"btn btn-default add-to-cart\"><i class=\"fa fa-shopping-cart\"></i>Add to cart</a>";
-            div += "</div>";
-            $('.tab-content').append(div);
-        }
-
-    })
+    // $.getJSON("url of search result",function (data) {
+    //     var product_image = data.image;
+    //     var product_description = data.description;
+    //     var product_price = data.price;
+    //
+    //     for(var i = 0; i<data.length;i++){
+    //         var div = "<div class=\"col-sm-2\">";
+    //         div += "<div class=\"product-image-wrapper\">";
+    //         div += " <div class=\"single-products\">";
+    //         div += "<div class=\"productinfo text-center\">";
+    //         div += "<a href=\"#\"><img alt = '' src='" + product_image + "'/></a>";
+    //         div += "<h2>" + product_price + "</h2>";
+    //         div += "<p>" + product_description + "</p>";
+    //         div += "<a href=\"#\" class=\"btn btn-default add-to-cart\"><i class=\"fa fa-shopping-cart\"></i>Add to cart</a>";
+    //         div += "</div>";
+    //         $('.tab-content').append(div);
+    //     }
+    //
+    // })
     for(var i = 0; i<4;i++){
         var div = "<div class=\"col-sm-2\">";
         div += "<div class=\"product-image-wrapper\">";
