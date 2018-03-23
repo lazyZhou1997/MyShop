480edeeda891fe55a0c7c7bf4b26cb5842a40c03
Vicent_Chen <1336754721@qq.com>
2018-03-23 19:14:44 +0800

    add product control

--- src/main/java/edu/scu/my_shop/controller/ProductController.java
+++ src/main/java/edu/scu/my_shop/controller/ProductController.java
@@ -1,13 +1,11 @@
 package edu.scu.my_shop.controller;
 
 import edu.scu.my_shop.entity.Product;
+import edu.scu.my_shop.service.FileService;
 import edu.scu.my_shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,9 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private FileService fileService;
 
     @PostMapping("/searchProductByName")
     @ResponseBody
@@ -29,4 +30,41 @@ public class ProductController {
 
         return "search-results";
     }
+
+    // TODO: add admin authority
+    @GetMapping("getAllProducts")
+    public List<Product> getAllProducts() {
+        List<Product> productList = productService.getAllProducts();
+        return productList;
+    }
+
+    @PostMapping("addProduct")
+    public String insertProduct(@RequestParam("product")Product product) {
+        productService.insertProduct(product);
+        return "";
+    }
+
+    @PostMapping("updateProductInfo")
+    @ResponseBody
+    public String updateProduct(@RequestParam("product")Product product) {
+        if (product == null || product.getProductId() == null ) {
+            // TODO: return to page or return string
+            return "未知商品";
+        }
+        productService.updateProduct(product);
+        return "";
+    }
+
+    @PostMapping("deleteProduct")
+    @ResponseBody
+    public String deleteProduct(@RequestParam("productID")String productID) {
+        if (productID == null) {
+            // TODO: return to page or return string
+            return "未知商品";
+        }
+        productService.deleteProduct(productID);
+        fileService.deleteAllProductImages(productID);
+        return "";
+    }
+
 }
